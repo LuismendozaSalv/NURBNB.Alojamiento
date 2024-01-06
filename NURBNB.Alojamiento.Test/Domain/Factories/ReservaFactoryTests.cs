ffb417ac702cfdc2a228d28689fe8557c37fa365
Luis Mendoza <luismendozasalv@gmail.com>
2024-01-06 10:25:56 -0400

    Add ReservaFactoryTests

---
+++ NURBNB.Alojamiento.Test/Domain/Factories/ReservaFactoryTests.cs
@@ -0,0 +1,30 @@
+﻿using NURBNB.Alojamiento.Domain.Factories;
+using NURBNB.Alojamiento.Domain.Model.Alojamiento;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NURBNB.Alojamiento.Test.Domain.Factories
+{
+	public class ReservaFactoryTests
+	{
+		[Fact]
+		public void Crear_ReservaInstanceConValoresCorrectos()
+		{
+			// Arrange
+			DateTime fechaEntrada = DateTime.Now;
+			DateTime fechaSalida = DateTime.Now;
+
+			ReservaFactory factory = new ReservaFactory();
+
+			// Act
+			Reserva reserva = factory.Crear(Guid.NewGuid(), fechaEntrada, fechaSalida, EstadoReserva.Pendiente);
+
+			// Assert
+			Assert.NotNull(reserva);
+			Assert.Equal(fechaEntrada, reserva.FechaEntrada);
+		}
+	}
+}
